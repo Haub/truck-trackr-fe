df172fa777abba0d36565f3ec8f11cc67b50071f
Freddie Levenson <freddielev@gmail.com>
2018-12-19 15:22:18 -0700

    Add all necessary fields to controlled form and add Toggle Sign up functionality

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -6,10 +6,15 @@ export class Login extends Component {
   constructor() {
     super()
     this.state = {
-      name: '',
       email: '',
       password: '',
-      signUp: false
+      signUp: false,
+      locationType: '',
+      businessName: '',
+      address: '',
+      phoneNumber: '',
+      contactName: '',
+      foodType: '',
     }
   }
 
@@ -30,15 +35,32 @@ export class Login extends Component {
   }
 
   render() {
+    const { signUp, locationType } = this.state
+    let showFoodType
+
+    if( signUp && locationType === 'food-truck' ){
+      showFoodType = true
+    }
+
     return(
       <div className='login-container'>
         <main className='login'>
           <form className='login-form' onSubmit={this.handleSubmit}>
             <input className='email-input' name='email' type='email' placeholder='Email' onChange={this.handleKeyPress} />
             <input className='password-input' name='password' type='password' placeholder='Password' onChange={this.handleKeyPress} />
-            <button className='signin-button'>Sign In</button>
+            <input className={ signUp ? 'business-name-input' : 'hidden' } name='businessName' placeholder='Business Name' onChange={this.handleKeyPress} />
+            <input className={ signUp ? 'address-input' : 'hidden' } name='address' placeholder='Address/City Located' onChange={this.handleKeyPress} />
+            <input className={ signUp ? 'phone-number-input' : 'hidden' } name='phoneNumber' placeholder='Phone Number' onChange={this.handleKeyPress} />
+            <input className={ signUp ? 'contact-name-input' : 'hidden' } name='contactName' placeholder='Contact Name' onChange={this.handleKeyPress} />
+            <input className={ showFoodType ? 'food-type-input' : 'hidden' } name='foodType' placeholder='Food Type' onChange={this.handleKeyPress} />
+            <p className={ signUp ? 'business-type' : 'hidden' }>What Type of Business:</p>
+            <input className={ signUp ? 'food-truck-radio' : 'hidden' } type='radio' name='locationType' value='food-truck' id='food-truck-button' onChange={this.handleKeyPress} />
+            <label className={ signUp ? 'food-truck-label' : 'hidden' } for='food-truck-button'>Food Truck</label>
+            <input className={ signUp ? 'brewery-radio' : 'hidden' } type='radio' name='locationType' value='brewery' id='brewery-button'onChange={this.handleKeyPress} />
+            <label className={ signUp ? 'brewery-label' : 'hidden' } for='brewery-button'>Brewery</label>
+            <button className='signin-button'>{ signUp ? 'Sign Up' : 'Sign In' }</button>
           </form>
-          <button onClick={this.toggleSignUp}>First Time Here? Sign Up!</button>
+          <button className='signup-button' onClick={this.toggleSignUp}>{ signUp ? 'Actually I already have an account' : 'First Time Here? Sign Up!'}</button>
         </main>
       </div>
 
