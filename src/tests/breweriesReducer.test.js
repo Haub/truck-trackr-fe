d29b5b12f6aa271c3b0b0ff5e48a9fc060e4fc65
Freddie Levenson <freddielev@gmail.com>
2018-12-29 15:14:25 -0700

    Add and pass test for returning breweries properly for breweriesReducer

--- src/tests/breweriesReducer.test.js
+++ src/tests/breweriesReducer.test.js
@@ -11,8 +11,17 @@ describe('breweriesReducer', () => {
 
     it('should return the default state', () => {
         const expected = [];
-        const result = breweriesReducer(undefined, {})
+        const result = breweriesReducer(undefined, {});
 
         expect(result).toEqual(expected);
     })
+
+    it('should return state with breweries', () => {
+        const initialState = [];
+        const expected = breweries;
+
+        const result = breweriesReducer(initialState, Actions.loadBreweries(breweries))
+        expect(result).toEqual(expected)
+    })
+
 })
