de4ae512a7a63e00ceb3c6cd3ac6cfd6e157c582
Freddie Levenson <freddielev@gmail.com>
2018-12-29 13:44:06 -0700

    Fix failing EDIT EVENT action test

--- src/tests/actions.test.js
+++ src/tests/actions.test.js
@@ -73,7 +73,7 @@ describe('actions', () => {
   });
 
   it ('should have a type of EDIT_EVENT', () => {
-    const profile = mockProfile;
+    const event = mockEvent;
     const expectedAction = {
       type: 'EDIT_EVENT', 
       event
