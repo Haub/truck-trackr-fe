edc2bc26a4ad8a340f6f6342de6053d570d4dab9
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-12-30 12:40:44 -0700

    Add fetch call to backend for breweries and foodTrucks pages.  We are currently getting a CORs error

---
+++ src/components/BusinessContainer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+
+export const BusinessContainer = () => {
+
+
+  return (
+    <div>
+      
+
+    </div>   
+  )
+  
+};
+
+export default withRouter(BusinessContainer);
