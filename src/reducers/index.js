d872317ba81f2f61fe984859c23117acf0a56ff7
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2018-12-30 12:13:33 -0700

    Add all reducers into combineReducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,14 @@
-import {combineReducers} from 'redux'
+import {combineReducers} from 'redux';  
+import {breweriesReducer} from './breweriesReducer';
+import {currentPageReducer} from './currentPageReducer';
+import {foodTrucksReducer} from './foodTrucksReducer';
+import {userEventsReducer} from './userEventsReducer';
+import {userReducer} from './userReducer';
 
 export const rootReducer = combineReducers({
-
-})
+  breweries: breweriesReducer,
+  currentPage: currentPageReducer,
+  foodTrucks: foodTrucksReducer, 
+  userEvents: userEventsReducer,
+  user: userReducer 
+});
