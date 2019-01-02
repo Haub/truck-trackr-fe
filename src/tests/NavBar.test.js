90c727abed805695f97cc1778372dc132f8bb935
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-01-02 14:35:20 -0700

    Add mdtp tests for NavBar

--- src/tests/NavBar.test.js
+++ src/tests/NavBar.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { NavBar } from '../containers/NavBar/';
+import { mapDispatchToProps } from '../containers/NavBar';
+import { mockBreweries, mockFoodTrucks, mockProfile } from '../utilities/mockData.js';
 
 
 describe ('NAVBAR', () => {
@@ -8,4 +10,29 @@ describe ('NAVBAR', () => {
     const wrapper = shallow(<NavBar />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch when loadBreweries is invoked', () => {
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.loadBreweries(mockBreweries);
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+
+    it('should call dispatch when loadFoodTrucks is invoked', () => {
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.loadFoodTrucks(mockFoodTrucks);
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+
+    it('should call dispatch when loadProfile is invoked', () => {
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.loadProfile(mockProfile);
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+
+  });
+
 })
