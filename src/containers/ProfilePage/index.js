3f21ed1c1a417ce53292d05cf0b99ab773a24f69
Freddie Levenson <freddielev@gmail.com>
2019-01-01 19:52:40 -0700

    Begin construction of profilePage component

--- src/containers/ProfilePage/index.js
+++ src/containers/ProfilePage/index.js
@@ -3,10 +3,40 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import './ProfilePage.css'
 
-export const ProfilePage = (props) => {
+export class ProfilePage extends Component {
 
-    return(
-        <div>
-        </div>
-    )
+    render(){
+        const { currentPage } = this.props
+        console.log(currentPage)
+        if(!Object.keys(currentPage).length){
+            return(
+                <div></div>
+            )
+        } else if(currentPage && currentPage.data.type === 'food_truck'){
+            const { attributes } = this.props.currentPage.data
+            return(
+                <div>
+                    <h3>{attributes.name}</h3>
+                    <h4>Food Type: {attributes.food_type}</h4>
+                </div>
+            )
+        } else if (currentPage && currentPage.data.type === 'brewery'){
+            const { attributes } = this.props.currentPage.data
+            return(
+                <div>
+                </div>
+            )
+        } else {
+            return(
+                <div>
+                </div>
+            )
+        }
+    }
 }
+
+export const mapStateToProps = (state) => ({
+    currentPage: state.currentPage
+})
+
+export default connect(mapStateToProps, null)(ProfilePage)
