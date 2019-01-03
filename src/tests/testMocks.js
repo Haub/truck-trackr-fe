718f209efbcdbfc9da43cbbd719f843ee99a2e07
Freddie Levenson <freddielev@gmail.com>
2019-01-03 16:51:52 -0700

    Add sad path test for loginUser

--- src/tests/testMocks.js
+++ src/tests/testMocks.js
@@ -1,173 +1,177 @@
 export const mockBreweries = {
-  "data": [
-            {
-              "id": "1",
-              "type": "brewery",
-              "attributes": {
-                  "name": "Lockman Inc"
-              }
-            },
-            {
-              "id": "2",
-              "type": "brewery",
-              "attributes": {
-                  "name": "Cremin, Nikolaus and Volkman"
-              }
-            }
-          ]
-};
- 
-  export const mockBrewery = {
-    "data": [
-      {
-        "id": "1",
-        "type": "brewery",
-        "attributes": {
-          "name": "Heads Of State",
-          "address": "Risotto with Seafood",
-          "contact_name": "Pamala",
-          "phone": "344.694.9247",
-          "email": "young@champlin.biz",
-          "website": "http://hosbeer.com",
-          "logo_image": "https://pigment.github.io/fake-logos/logos/medium/color/11.png"
-        }
-      }
-    ]
-  };
- 
-  export const mockFoodTrucks = [
+  data: [
     {
-      "data": [
-        {
-          "id": "1",
-          "type": "food_truck",
-          "attributes": {
-            "name": "Hot Pasta"
-          }
-        },
-        {
-          "id": "2",
-          "type": "food_truck",
-          "attributes": {
-              "name": "Hell On Wheels",
-          }
-        }
-      ]
+      id: "1",
+      type: "brewery",
+      attributes: {
+        name: "Lockman Inc"
+      }
+    },
+    {
+      id: "2",
+      type: "brewery",
+      attributes: {
+        name: "Cremin, Nikolaus and Volkman"
+      }
     }
   ]
- 
-  export const mockFoodTruck = {
-    "data": [
+};
+
+export const mockBrewery = {
+  data: [
+    {
+      id: "1",
+      type: "brewery",
+      attributes: {
+        name: "Heads Of State",
+        address: "Risotto with Seafood",
+        contact_name: "Pamala",
+        phone: "344.694.9247",
+        email: "young@champlin.biz",
+        website: "http://hosbeer.com",
+        logo_image:
+          "https://pigment.github.io/fake-logos/logos/medium/color/11.png"
+      }
+    }
+  ]
+};
+
+export const mockFoodTrucks = [
+  {
+    data: [
       {
-        "id": "1",
-        "type": "food_truck",
-        "attributes": {
-          "name": "Hot Pasta",
-          "food_type": "Risotto with Seafood",
-          "contact_name": "Pamala",
-          "phone": "344.694.9247",
-          "email": "young@champlin.biz",
-          "website": "http://hopo.com",
-          "logo_image": "https://pigment.github.io/fake-logos/logos/medium/color/11.png"
+        id: "1",
+        type: "food_truck",
+        attributes: {
+          name: "Hot Pasta"
+        }
+      },
+      {
+        id: "2",
+        type: "food_truck",
+        attributes: {
+          name: "Hell On Wheels"
         }
       }
     ]
-  };
- 
-  export const mockProfile = {
-    "data": [
-      {
-        "id": "1",
-        "type": "brewery",
-        "attributes": {
-          "name": "Heads Of State",
-          "address": "Risotto with Seafood",
-          "contact_name": "Pamala",
-          "phone": "344.694.9247",
-          "email": "young@champlin.biz",
-          "website": "http://hosbeer.com",
-          "logo_image": "https://pigment.github.io/fake-logos/logos/medium/color/11.png"
-        }
+  }
+];
+
+export const mockFoodTruck = {
+  data: [
+    {
+      id: "1",
+      type: "food_truck",
+      attributes: {
+        name: "Hot Pasta",
+        food_type: "Risotto with Seafood",
+        contact_name: "Pamala",
+        phone: "344.694.9247",
+        email: "young@champlin.biz",
+        website: "http://hopo.com",
+        logo_image:
+          "https://pigment.github.io/fake-logos/logos/medium/color/11.png"
       }
-    ]
-  };
- 
-  export const mockEvent = {
-   id: 435,
-   date: '04/06/2018',
-   truck_booked: true,
-   brewery_id: 105
- };
- 
-  export const mockUserEvents = [
-   {
-     id: 433,
-     date: '04/03/2018',
-     truck_booked: true,
-     brewery_id: 105
-   },
-   {
+    }
+  ]
+};
+
+export const mockProfile = {
+  data: [
+    {
+      id: "1",
+      type: "brewery",
+      attributes: {
+        name: "Heads Of State",
+        address: "Risotto with Seafood",
+        contact_name: "Pamala",
+        phone: "344.694.9247",
+        email: "young@champlin.biz",
+        website: "http://hosbeer.com",
+        logo_image:
+          "https://pigment.github.io/fake-logos/logos/medium/color/11.png"
+      }
+    }
+  ]
+};
+
+export const mockEvent = {
+  id: 435,
+  date: "04/06/2018",
+  truck_booked: true,
+  brewery_id: 105
+};
+
+export const mockUserEvents = [
+  {
+    id: 433,
+    date: "04/03/2018",
+    truck_booked: true,
+    brewery_id: 105
+  },
+  {
     id: 434,
-    date: '04/08/2018',
+    date: "04/08/2018",
+    truck_booked: false,
+    brewery_id: 105
+  }
+];
+
+export const mockUpdatedEvents = [
+  {
+    id: 433,
+    date: "04/03/2018",
+    truck_booked: true,
+    brewery_id: 105
+  },
+  {
+    id: 434,
+    date: "04/08/2018",
     truck_booked: false,
     brewery_id: 105
   },
- ]
-
- export const mockUpdatedEvents = [
-    {
-        id: 433,
-        date: '04/03/2018',
-        truck_booked: true,
-        brewery_id: 105
-      },
-      {
-       id: 434,
-       date: '04/08/2018',
-       truck_booked: false,
-       brewery_id: 105
-     }, 
-     {
-        id: 435,
-        date: '04/06/2018',
-        truck_booked: true,
-        brewery_id: 105
-      }
- ]
-
- export const mockEventToEdit = {
-    id: 434,
-    date: '04/03/2018',
+  {
+    id: 435,
+    date: "04/06/2018",
     truck_booked: true,
     brewery_id: 105
- }
+  }
+];
 
- export const mockEditedEvents = [
-    {
-        id: 433,
-        date: '04/03/2018',
-        truck_booked: true,
-        brewery_id: 105
-    },
-    {
-       id: 434,
-       date: '04/03/2018',
-       truck_booked: true,
-       brewery_id: 105
-    },
- ]
+export const mockEventToEdit = {
+  id: 434,
+  date: "04/03/2018",
+  truck_booked: true,
+  brewery_id: 105
+};
 
- export const mockLoginUser = {
-    uid: 55,
-    account_type: 'brewery'
- }
+export const mockEditedEvents = [
+  {
+    id: 433,
+    date: "04/03/2018",
+    truck_booked: true,
+    brewery_id: 105
+  },
+  {
+    id: 434,
+    date: "04/03/2018",
+    truck_booked: true,
+    brewery_id: 105
+  }
+];
 
- export const mockPostParams = [
-  'https://truck-trackr-api.herokuapp.com/login', {
-    method: 'POST',
-    body: JSON.stringify( mockLoginUser ),
+export const mockLoginUser = {
+  uid: 55,
+  account_type: "brewery"
+};
+
+export const mockPostParams = [
+  "https://truck-trackr-api.herokuapp.com/login",
+  {
+    method: "POST",
+    body: JSON.stringify(mockLoginUser),
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json"
     }
   }
-]
+];
--- src/tests/utilities.test.js
+++ src/tests/utilities.test.js
@@ -1,76 +1,104 @@
-import * as helper from '../utilities'
-import { mockBreweries, mockFoodTrucks, mockProfile, mockLoginUser, mockPostParams } from './testMocks'
+import * as helper from "../utilities";
+import {
+  mockBreweries,
+  mockFoodTrucks,
+  mockProfile,
+  mockLoginUser,
+  mockPostParams
+} from "./testMocks";
 
-describe('helper', () => {
-    describe('fetchBusinesses', () => {
-        beforeEach(() => {      
-            window.fetch = jest.fn(() => Promise.resolve({
-              ok: true,
-              json: () => Promise.resolve()
-            }))
-          })
-        
-          it('should call fetch with the correct url if locationType is breweries', async () => {
-              const expected = 'https://truck-trackr-api.herokuapp.com/api/v1/breweries'
-
-              await helper.fetchBusinesses('breweries')
-
-              expect(window.fetch).toHaveBeenCalledWith(expected)
-          })
-
-          it('should call fetch with the correct url if locationType is food_trucks', async () => {
-            const expected = 'https://truck-trackr-api.herokuapp.com/api/v1/food_trucks'
-
-            await helper.fetchBusinesses('food_trucks')
-
-            expect(window.fetch).toHaveBeenCalledWith(expected)
+describe("helper", () => {
+  describe("fetchBusinesses", () => {
+    beforeEach(() => {
+      window.fetch = jest.fn(() =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve()
         })
-        
-          it('should return breweries if the status is ok', async () => {
-            window.fetch = jest.fn(() => Promise.resolve({
-                ok: true,
-                json: () => Promise.resolve(mockBreweries)
-              }))
+      );
+    });
 
-            const expected = mockBreweries;
-            const result = await helper.fetchBusinesses('breweries');
-            expect(result).toEqual(expected)
-          })
+    it("should call fetch with the correct url if locationType is breweries", async () => {
+      const expected =
+        "https://truck-trackr-api.herokuapp.com/api/v1/breweries";
 
-          it('should return foodTrucks if the status is ok', async () => {
-            window.fetch = jest.fn(() => Promise.resolve({
-                ok: true,
-                json: () => Promise.resolve(mockFoodTrucks)
-              }))
+      await helper.fetchBusinesses("breweries");
 
-            const expected = mockFoodTrucks;
-            const result = await helper.fetchBusinesses('food_trucks');
-            expect(result).toEqual(expected)
-          })
+      expect(window.fetch).toHaveBeenCalledWith(expected);
+    });
 
-          it('should throw an error if status is not ok', async () => {
-            window.fetch = jest.fn(() => Promise.resolve({ ok: false, statusText: 'fetch failed.'}))
+    it("should call fetch with the correct url if locationType is food_trucks", async () => {
+      const expected =
+        "https://truck-trackr-api.herokuapp.com/api/v1/food_trucks";
 
-            const expected = Error('fetch failed.')
+      await helper.fetchBusinesses("food_trucks");
 
-            expect(helper.fetchBusinesses()).rejects.toEqual(expected)
-          })
-    })
+      expect(window.fetch).toHaveBeenCalledWith(expected);
+    });
 
-    describe('loginUser', () => {
-        beforeEach(()=> {
-            window.fetch = jest.fn(() => Promise.resolve({
-                ok: true,
-                json: () => Promise.resolve(mockProfile)
-              }))
+    it("should return breweries if the status is ok", async () => {
+      window.fetch = jest.fn(() =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockBreweries)
         })
+      );
 
-        it('should call fetch with the correct URL', async () => {
-            const expected = mockPostParams;
+      const expected = mockBreweries;
+      const result = await helper.fetchBusinesses("breweries");
+      expect(result).toEqual(expected);
+    });
 
-            await helper.loginUser(mockLoginUser)
-
-            expect(window.fetch).toHaveBeenCalledWith(...expected)
+    it("should return foodTrucks if the status is ok", async () => {
+      window.fetch = jest.fn(() =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockFoodTrucks)
         })
-    })
-})
+      );
+
+      const expected = mockFoodTrucks;
+      const result = await helper.fetchBusinesses("food_trucks");
+      expect(result).toEqual(expected);
+    });
+
+    it("should throw an error if status is not ok", async () => {
+      window.fetch = jest.fn(() =>
+        Promise.resolve({ ok: false, statusText: "fetch failed." })
+      );
+
+      const expected = Error("fetch failed.");
+
+      expect(helper.fetchBusinesses()).rejects.toEqual(expected);
+    });
+  });
+
+  describe("loginUser", () => {
+    beforeEach(() => {
+      window.fetch = jest.fn(() =>
+        Promise.resolve({
+          ok: true,
+          json: () => Promise.resolve(mockProfile)
+        })
+      );
+    });
+
+    it("should call fetch with the correct URL", async () => {
+      const expected = mockPostParams;
+
+      await helper.loginUser(mockLoginUser);
+
+      expect(window.fetch).toHaveBeenCalledWith(...expected);
+    });
+
+    it("throws an error if the response is not OK", async () => {
+      const expected = Error("user not found");
+
+      window.fetch = jest.fn(() =>
+        Promise.resolve({ ok: false, statusText: "user not found" })
+      );
+
+      expect(helper.loginUser()).rejects.toEqual(expected);
+    });
+  });
+});
