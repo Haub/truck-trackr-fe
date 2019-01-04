396219cc9d398ec547e75cf42dc2dc45cf532d56
Freddie Levenson <freddielev@gmail.com>
2019-01-03 18:13:45 -0700

    add necessary configuration for travis firebase

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,9 +2,18 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import { config } from '../../utilities/config.js';
 
+let configuration = config || {
+  apiKey: process.env.apiKey,
+  auhDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  messagingSenderId: process.env.messagingSenderId,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket
+}
+
 class Firebase {
   constructor() {
-    app.initializeApp(config);
+    app.initializeApp(configuration);
 
     this.auth = app.auth();
   }
