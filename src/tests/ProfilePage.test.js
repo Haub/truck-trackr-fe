673f7be29f5aa2f4f729e479a1df0e4f5985a377
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-01-06 18:42:48 -0700

    Add tests in profilePage for mapDispatchToProps

--- src/tests/ProfilePage.test.js
+++ src/tests/ProfilePage.test.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { ProfilePage, mapStateToProps } from "../containers/ProfilePage/";
-import { mockProfile, mockUser } from "./testMocks";
+import { ProfilePage, mapStateToProps, mapDispatchToProps } from "../containers/ProfilePage/";
+import { mockProfile, mockUser, mockEvent, mockUserEvents } from "./testMocks";
 
 describe("PROFILE PAGE", () => {
   it("should match the snapshot", () => {
     const wrapper = shallow(
-      <ProfilePage currentPage={mockProfile.data} user={mockUser} />
+      <ProfilePage currentPage={mockProfile.data} user={mockUser} userEvents={mockUserEvents} />
     );
     expect(wrapper).toMatchSnapshot();
   });
@@ -14,11 +14,36 @@ describe("PROFILE PAGE", () => {
   describe("mapStateToProps", () => {
     it("should have access to currentUser", () => {
       const mockStore = {
-        currentPage: {}
+        currentPage: {},
+        user: {},
+        userEvents: {}
       };
       const expected = { ...mockStore };
       const result = mapStateToProps(mockStore);
       expect(result).toEqual(expected);
     });
   });
+
+  describe("mapDispatchToProps", () => {
+    it("should call dispatch when addUser is invoked", () => {
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.addUser(mockProfile);
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+
+    it("should call dispatch when editEvent is invoked", () => {
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.editEvent(mockEvent);
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+
+    it("should call dispatch when removeEvent is invoked", () => {
+      const mockDispatch = jest.fn();
+      const mappedProps = mapDispatchToProps(mockDispatch);
+      mappedProps.removeEvent(mockEvent);
+      expect(mockDispatch).toHaveBeenCalled();
+    });
+  })
 });
