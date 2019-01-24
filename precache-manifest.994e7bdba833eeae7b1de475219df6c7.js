8241c5394bb2353c59b5ef50fc74ad50a091f9fd
John DoeMeganHaubelt <meganhaubelt@gmail.com>
2019-01-24 12:58:31 -0700

    Updates

---
+++ precache-manifest.994e7bdba833eeae7b1de475219df6c7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9aaf7d5640a994169db23065a82e526",
+    "url": "/truck-trackr-fe/static/media/hamburger.d9aaf7d5.svg"
+  },
+  {
+    "revision": "28e4e20f77cf4e2527cf9612ec0a9c0d",
+    "url": "/truck-trackr-fe/static/media/food-truck.28e4e20f.png"
+  },
+  {
+    "revision": "976a538bdfe01df1b7d91a5af50aa0d2",
+    "url": "/truck-trackr-fe/static/media/barrel-icon-new.976a538b.png"
+  },
+  {
+    "revision": "5efe087d82da9d7bf9a0",
+    "url": "/truck-trackr-fe/static/js/runtime~main.5efe087d.js"
+  },
+  {
+    "revision": "ec69cf5a8c651a75de1e",
+    "url": "/truck-trackr-fe/static/js/main.ec69cf5a.chunk.js"
+  },
+  {
+    "revision": "1bec5009b394e9233634",
+    "url": "/truck-trackr-fe/static/js/1.1bec5009.chunk.js"
+  },
+  {
+    "revision": "ec69cf5a8c651a75de1e",
+    "url": "/truck-trackr-fe/static/css/main.ca3123e5.chunk.css"
+  },
+  {
+    "revision": "835cc80d4231f9332eb76280951ca4a7",
+    "url": "/truck-trackr-fe/index.html"
+  }
+];
